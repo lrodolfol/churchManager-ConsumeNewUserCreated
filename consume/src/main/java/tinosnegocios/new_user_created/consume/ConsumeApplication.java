45127d4059a832cebc6a14528c341f7598aa5d0a
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-12-15 13:30:07 -0300

    Implement a little schedule for read confirmed users

--- consume/src/main/java/tinosnegocios/new_user_created/consume/ConsumeApplication.java
+++ consume/src/main/java/tinosnegocios/new_user_created/consume/ConsumeApplication.java
@@ -10,16 +10,30 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 import tinosnegocios.new_user_created.consume.Models.UserCreated;
 import tinosnegocios.new_user_created.consume.Services.RabbitMqConsume;
+import tinosnegocios.new_user_created.consume.Services.SNSService;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class ConsumeApplication {
 
 	public static void main(String[] args) {
+		SNSService snsService = new SNSService();
+		System.out.println("Process initialized, waiting messages...");
+
+		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+		// Agendando uma tarefa para rodar a cada 5 segundos
+		scheduler.scheduleAtFixedRate(() -> {
+			snsService.searchAndSendEmailToConfirmedEmails();
+		}, 0, 5, TimeUnit.SECONDS);
+
 		RabbitMqConsume rabbitMqConsume = new RabbitMqConsume();
 		rabbitMqConsume.ConsumeMessages();
 	}
