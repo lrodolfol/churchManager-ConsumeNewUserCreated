72e9cbec764e3c46c8f16348fea92d20a14217ea
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-12-15 13:29:37 -0300

    Implement service into rabbitmq consume

--- consume/src/main/java/tinosnegocios/new_user_created/consume/Services/ProcessReadMessage.java
+++ consume/src/main/java/tinosnegocios/new_user_created/consume/Services/ProcessReadMessage.java
@@ -8,7 +8,13 @@ public class ProcessReadMessage {
         Gson gson = new Gson();
 
         try{
+            SNSService snsService = new SNSService();
+
             UserCreated user = gson.fromJson(message, UserCreated.class);
+
+            snsService.SubscribeTopis(user);
+
+
             System.out.println("E-mail enviado para " + user.getEmailAddress());
             return true;
         }catch (Exception ex){
